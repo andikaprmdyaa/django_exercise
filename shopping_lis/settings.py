61c3f36adc48b8539efa10efa107aac57117f8df
andikaprmdyaa <andikapramudya30@gmail.com>
2023-09-07 10:53:27 +0700

    Tutorial 1 completed

--- shopping_lis/settings.py
+++ shopping_lis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*&&5xb)y4#24j)s(^al*^3$&u@#(wi9++3x*g35_f%r1o_thuf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
